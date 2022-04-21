601192f4a5ef75136057bdf16b583d9fbc924554
Srikanth Reddy Bethi <srikanthreddy.bethi@gmail.com>
2022-04-21 10:00:48 +0100

    Updated metaflow UI to v1.1.2 and metaflow service to v2.2.4 for bug fixes and new features

--- modules/ui/variables.tf
+++ modules/ui/variables.tf
@@ -113,7 +113,7 @@ variable "ui_backend_container_image" {
 
 variable "ui_static_container_image" {
   type        = string
-  default     = "public.ecr.aws/outerbounds/metaflow_ui:v1.0.1"
+  default     = "public.ecr.aws/outerbounds/metaflow_ui:v1.1.2"
   description = "Container image for the UI frontend app"
 }
 
